f857478cb2c55dd9d934198364f53b436c3beafc
prakash-k <k.prakash@novars.jp>
2026-06-23 11:56:47 +0900

    Deploy compiled Flutter web app to domain root on main

--- textiles_app/build/web/flutter_bootstrap.js
+++ textiles_app/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2355718597"
+    serviceWorkerVersion: "2207285757"
   }
 });
